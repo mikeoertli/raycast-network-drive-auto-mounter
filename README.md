bdd35893c9d6224e1898642400eb810d1906a514
Mike Oertli <mikeoertli@gmail.com>
2025-02-26 09:41:55 -0700

    initial version of script

---
+++ README.md
@@ -0,0 +1,31 @@
+# Raycast Network Drive Auto-Mount Utility
+
+<p align="center">
+<img width="192" alt="Light/Dark Mode Icon" src="images/disk-icon.png"/>
+</p>
+
+This script command verifies network drives are mounted, and if not found, mounts them via finder (allowing for credential
+storage in keychain).
+
+## Configuration
+
+There is required configuration, specifically a config file like the [example](./auto_smb_mounts.txt) (shown below) located at
+the default path of `$HOME/.config/raycast/auto_smb_mounts.txt`.
+
+```
+smb://mynas/share1
+smb://my-other-nas/storage_share
+smb://mynas/share2
+
+```
+
+(Note the newline after the last entry.)
+
+### Add Custom Script Directory
+
+Once cloned, make sure to add a custom script directory in Raycast. To do this, the information from the
+[Raycast Script Commands](https://github.com/raycast/script-commands) repo applies here too.
+
+## More Info
+
+Read more about Raycast Script Commands [here](https://github.com/raycast/script-commands).
---
+++ network-drive-auto-mounter.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Network Drive Auto-Mounter
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon images/disk-icon.png
+# @raycast.packageName Utilities
+# @raycast.argument1 { "type": "text", "placeholder": "override config file", "percentEncoded": true, "optional": true }
+
+# Documentation:
+# @raycast.description Verifies each network drive specified in the config file (located at "~/.config/raycast/auto_smb_mounts.txt"), and if not mounted, mounts it using finder.
+# @raycast.author Mike Oertli
+# @raycast.authorURL https://github.com/mikeoertli
+
+
+# Default file is located in: '~/.config/raycast/auto_smb_mounts.txt'
+# The format of the document is as follows...
+#   - it should contain a newline separated list of SMB mount paths
+#   - no quotes
+#   - there needs to be a new line after the last entry otherwise it will be ignored
+
+DEFAULT_CONFIG_FILE="$HOME/.config/raycast/auto_smb_mounts.txt"
+
+configFile=${1:-"$DEFAULT_CONFIG_FILE"}
+if [[ ! -f $configFile ]]; then
+  echo "🚨 Unable to find the specified config file: $configFile"
+  exit 1
+fi
+
+# Array of SMB share URLs, each will mount to /Volumes/ in a directory matching the share name
+# Populated by parsing the config file
+smbURLs=()
+
+# Read the file line by line.
+while IFS= read -r line; do
+  smbURLs+=("$line")
+done < "$configFile"
+
+function attempt_mount() {
+  local smbUrl=${1:?"Must provide a SMB mount path!"}
+  local mntName
+  mntName=$(basename "$smbUrl")
+  local volPath="/Volumes/${mntName}"
+
+  if [ -d "$volPath" ]; then
+    # echo >&2 "Already mounted: ${volPath} for ${mntName}"
+    echo -e "👌 ${mntName} already mounted"
+  else
+    # echo >&2 "Attempting to mount: ${volPath} for ${mntName}"
+    # This will open the SMB share in Finder, using stored keychain credentials.
+    open "${smbUrl}"
+    # Give the system a moment to mount
+    sleep 2
+    [ -d "$volPath" ] && echo -e "✅ ${mntName} mounted" || echo -e "❌ ${mntName} could not be mounted"
+  fi
+}
+
+# Loop through each network share
+for url in "${smbURLs[@]}"; do
+  attempt_mount "$url"
+done
+
